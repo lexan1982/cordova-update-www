d4c72c9bc93e50f65c2d1357a67cecb2c04ba260
lexan1982 <lexan1982@gmail.com>
2014-08-08 17:15:51 +0300

    clean

--- src/ios/Echo.h
+++
@@ -1,9 +0,0 @@
-/********* Echo.h Cordova Plugin Header *******/
-
-#import <Cordova/CDV.h>
-
-@interface Echo : CDVPlugin
-
-- (void)echo:(CDVInvokedUrlCommand*)command;
-
-@end
--- www/EchoPlugin.js
+++
@@ -1,5 +0,0 @@
-window.echo = function(str, callback) {
-    cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
-    }, "Echo", "echo", [str]);
-};
