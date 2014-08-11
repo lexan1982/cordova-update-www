f441ec4cfe3d99fdd96cc66dd98d3049668a5b84
lexan1982 <lexan1982@gmail.com>
2014-08-11 12:57:41 +0300

    clean

--- www/updateToVersion.js
+++ www/updateToVersion.js
@@ -1,4 +1,7 @@
-var updateToVersion = function(str, callback) {
+var updateToVersion = function (str, callback) {
+
+    console.log('..!! call plugin js: ' + str);
+
     cordova.exec(callback, function(err) {
         callback('error');
     }, "UpdateToVersion ", "echo", [str]);
