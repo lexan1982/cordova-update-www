d0450c1d67306b56fc1ebdb0809caaf48620a8d3
OLEKSANDR LUCHYTSKYI <lexan1982@gmail.com>
2014-08-11 09:43:19 +0300

    Update updateToVersion.js

--- www/updateToVersion.js
+++ www/updateToVersion.js
@@ -1,5 +1,7 @@
-window.updateToVersion = function(str, callback) { 
+var updateToVersion = function(str, callback) {
     cordova.exec(callback, function(err) {
         callback('error');
-    }, "UpdateToVersion", "echo", [str]);
+    }, "Echo", "echo", [str]);
 };
+
+module.exports = updateToVersion;
