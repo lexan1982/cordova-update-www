7accad1dea025be5fca28e6e047f49debe59fc38
OLEKSANDR LUCHYTSKYI <lexan1982@gmail.com>
2015-02-05 01:17:28 -0800

    Update UartViewController.m

--- src/ios/UartViewController.m
+++ src/ios/UartViewController.m
@@ -31,10 +31,97 @@
 
 @implementation UartViewController
 
+@synthesize activityIndicator;
+
+- (void) startAnimation
+{
+    aCnt++;
+    if (!self.activityIndicator) {
+        //        self.activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame: CGRectMake(window.bounds.size.width / 2 - 36, window.bounds.size.height / 2 - 36, 72, 72)];
+        self.activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame: CGRectMake(5, 3, 36, 36)];
+        self.activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
+        self.activityIndicator.hidesWhenStopped = YES;
+        self.activityIndicator.autoresizingMask = (UIViewAutoresizingFlexibleLeftMargin |
+                                                   UIViewAutoresizingFlexibleRightMargin |
+                                                   UIViewAutoresizingFlexibleTopMargin |
+                                                   UIViewAutoresizingFlexibleBottomMargin);
+        [self.view addSubview: self.activityIndicator];
+        NSLog(@" activity is at %@", NSStringFromCGRect(self.activityIndicator.frame));
+    }
+    
+    int updMsgWidth = 205;
+    int updMsgFont = 18;
+    int updMsgHeight = 50;
+
+    UIDevice* thisDevice = [UIDevice currentDevice];
+    if(thisDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad)
+    {
+        updMsgWidth = 270;
+        updMsgFont = 24;
+        updMsgHeight = 70;
+    }
+    
+    
+    CGRect viewBounds = [[UIScreen mainScreen] applicationFrame];
+    updMsgView = [[UIView alloc] initWithFrame:CGRectMake(0.5*viewBounds.size.width - updMsgWidth/2, 0.5*viewBounds.size.height - updMsgHeight/2, updMsgWidth, updMsgHeight)];
+    
+    [updMsgView setBackgroundColor:[UIColor clearColor]];
+    updMsgView.tag = 102;
+    UILabel* chLbl = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, updMsgWidth, updMsgHeight)];
+    [chLbl setText:@"Please wait while your\napplication is updated..."];
+    [chLbl setBackgroundColor:[UIColor clearColor]];
+    [chLbl setTextColor:[UIColor blackColor]];
+    [chLbl setFont:[UIFont boldSystemFontOfSize:updMsgFont]];
+    chLbl.lineBreakMode = NSLineBreakByWordWrapping;
+    chLbl.numberOfLines = 0;
+    [updMsgView addSubview:chLbl];
+    
+    
+    [self.activityIndicator startAnimating];
+    self.view.userInteractionEnabled = NO;
+    self.webView.alpha = 0.2;
+    UIApplication *application = [UIApplication sharedApplication];
+    application.networkActivityIndicatorVisible = YES;
+    
+    [self.view addSubview:updMsgView];
+}
+
+
+/* show the user that loading activity has stopped */
+
+- (void) stopAnimation
+{
+    if (updMsgView) {
+        
+        [updMsgView removeFromSuperview];
+        updMsgView = nil;
+  
+    }
+    
+    UIView* updView = [self.view viewWithTag:102];
+    
+    if (updView) {
+        
+        [updView removeFromSuperview];
+    }
+    
+    [self.activityIndicator stopAnimating];
+    self.view.userInteractionEnabled = YES;
+    self.webView.alpha = 1.0;
+    UIApplication *application = [UIApplication sharedApplication];
+    application.networkActivityIndicatorVisible = NO;
+    //self.activityIndicator = nil;
+    
+    aCnt--;
+}
+
 - (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
+        
+        updMsgView = nil;
+        aCnt = 0;
       
     }
     return self;
@@ -57,8 +144,9 @@
 
 - (void) viewDidLoad
 {
+    
     [super viewDidLoad];
-  /*if (self.startFilePath.length > 0)
+     /*if (self.startFilePath.length > 0)
   {
     [self.webView stopLoading];      
     NSURLRequest *appReq = [NSURLRequest requestWithURL:[NSURL fileURLWithPath:self.startFilePath] cachePolicy: NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:20.0];
@@ -171,12 +259,14 @@
     NSString* isAppReload = [[NSUserDefaults standardUserDefaults] stringForKey:@"_isAppReload"];
     if ([isAppReload isEqualToString:@"T"]) {
         NSString* jsString = [NSString stringWithFormat:@"UART.app.getController('ToolsController').clearVersionFilesCache();"];
-        NSString *jsReturn = [theWebView stringByEvaluatingJavaScriptFromString:jsString];
-        NSLog(@"jsReturn (clear cache) : %@", jsReturn);
+//        NSString *jsReturn = [theWebView stringByEvaluatingJavaScriptFromString:jsString];
+//        NSLog(@"jsReturn (clear cache) : %@", jsReturn);
         [[NSUserDefaults standardUserDefaults] setObject:@"F" forKey:@"_isAppReload"];
     }
     
    /* [NSTimer scheduledTimerWithTimeInterval:2.5 target:[[UIApplication sharedApplication] delegate] selector:@selector(removeSplash:) userInfo:nil repeats:NO];*/
+    
+        //[self stopAnimation];
     
 	return [super webViewDidFinishLoad:theWebView];
 }
@@ -187,22 +277,25 @@
     // UART.app.getController('ToolsController').clearVersionFilesCache();
     NSString* isAppReload = [[NSUserDefaults standardUserDefaults] stringForKey:@"_isAppReload"];
     if ([isAppReload isEqualToString:@"T"]) {
-        NSString* jsString = [NSString stringWithFormat:@"TSVB.app.getController('ToolsController').clearVersionFilesCache();"];
+        NSString* jsString = [NSString stringWithFormat:@"UART.app.getController('ToolsController').clearVersionFilesCache();"];
         NSString *jsReturn = [theWebView stringByEvaluatingJavaScriptFromString:jsString];
         NSLog(@"jsReturn (clear cache) : %@", jsReturn);
         [[NSUserDefaults standardUserDefaults] setObject:@"F" forKey:@"_isAppReload"];
     }
+    
+    [self stopAnimation];
 	return [super webView:theWebView didFailLoadWithError:error];
 }
 
 /* Comment out the block below to over-ride */
-/*
+
 
 - (void) webViewDidStartLoad:(UIWebView*)theWebView 
 {
+    //[self startAnimation];
 	return [super webViewDidStartLoad:theWebView];
 }
-
+/*
 - (void) webView:(UIWebView*)theWebView didFailLoadWithError:(NSError*)error 
 {
 	return [super webView:theWebView didFailLoadWithError:error];
